5505a647e4113fafca42b9f6ebfa9337b44775d3
Voronova Nadezhda <voronovank@yandex.ru>
2016-05-04 00:36:39 +0300

    final version

--- for4.cpp
+++ for4.cpp
@@ -34,22 +34,6 @@ bool check(int a, int b, int c, point cur, int k, int fi)
   return ((a+b*cur.x1+c*cur.x2)%k == fi);
 }
 
-//печать результатов проверки для функции с координатами (a, b, c)
-//в файл "out.txt", точки A, B, C - точки, найденные во время проверки
-void print_point(int a, int b, int c, point A, point B, point C, bool err)
-{
-  ofstream f;
-  f.open("out.txt", ios::app);
-  f << "(" << a << ", " << b << ", " << c << ")  ";
-  f << A.x1 << " " << A.x2 << endl;
-  f << "           ";
-  f << B.x1 << " " << B.x2 << endl;
-  f << "           ";
-  f << C.x1 << " " << C.x2 << endl;
-  if (!err) f << "ERROR!!!" << endl;
-  f.close();
-}
-
 
 //проверка принадлежности точек одной прямой в R^2
 bool one_line(point A,  point B, point C, int k)
@@ -59,35 +43,29 @@ bool one_line(point A,  point B, point C, int k)
   return ((C.x1 - A.x1)*(B.x2 - A.x2) == (B.x1 - A.x1)*(C.x2 - A.x2));
 }
 
-//отладочная функция - проверка правильности считывания
+//вывод универсальной функции при её наличии
 void print_fi(vector <int> fi, int n)
 {
-    ofstream f;
-    f.open("out_fi_4.txt", ios::app);
-    f << endl << n;
-    //for (int i = 0; i < k; i++)
-        for (int j = 0; j < n; j++)
-        {
-            f << endl;
-            for (int k = 0; k < n; k++)
-            if (fi[j*n + k] != -1) f << fi[j*n + k] << ' ';
-            else f << "- ";
-        }
-    f << endl << endl;
-    f.close();
+  ofstream f;
+  f.open("out_fi_4.txt", ios::app);
+  f << endl << n;
+  for (int j = 0; j < n; j++)
+  {
+    f << endl;
+    for (int k = 0; k < n; k++)
+      if (fi[j*n + k] != -1) f << fi[j*n + k] << ' ';
+      else f << "- ";
+  }
+  f << endl << endl;
+  f.close();
 }
 
 //проверка функции fi на универсальность
 bool check_fi(vector<int> fi, int k, int n)
 {
-    int l1, l2, l0;
-    bool rez_of_check = true;
-    vector<int> save_rez(k*k, 0);
-//    ifstream f;
-//    f.open("out.txt");
-//    f.close();
-//  ofstream out_debug_wtf;
-//  out_debug_wtf.open("out_wtf.txt");
+  int l1, l2, l0;
+  bool rez_of_check = true;
+  vector<int> save_rez(k*k, 0);
   bool find = false;
   for (l0 = 0; l0 < k; l0++)
   for (l1 = 0; l1 < k; l1++)
@@ -99,46 +77,35 @@ bool check_fi(vector<int> fi, int k, int n)
     {
       point current1(i/k, i%k);
       if (check(l0, l1, l2, current1, k, fi[i]))
-      {A = current1;
-      for (int j = i+1; (j < (k*k))&&(!find); j++)
       {
-        point current2(j/k, j%k);
-        if (check(l0, l1, l2, current2, k, fi[j]))
-        {B = current2;
-        for (int l = j+1; (l < (k*k))&&(!find); l++)
+		A = current1;
+        for (int j = i+1; (j < (k*k))&&(!find); j++)
         {
-          point current3(l/k, l%k);
-          if ((check(l0, l1, l2, current3, k, fi[l]))&&(!one_line(A, B, current3, k)))
-          {C = current3;
-//            print_point(l0, l1, l2, A, B, C, true);
-            find = true;
-            save_rez[i]++; save_rez[j]++; save_rez[l]++;// save_rez[t]++;
+          point current2(j/k, j%k);
+          if (check(l0, l1, l2, current2, k, fi[j]))
+          {
+			B = current2;
+            for (int l = j+1; (l < (k*k))&&(!find); l++)
+            {
+              point current3(l/k, l%k);
+              if ((check(l0, l1, l2, current3, k, fi[l]))&&(!one_line(A, B, current3, k)))
+              {
+				C = current3;
+                find = true;
+                save_rez[i]++; save_rez[j]++; save_rez[l]++;
+              }
             }
           }
-        }}}}
+        }
+      }
+    }
 
     if ((A.x1==-1)||(B.x1==-1)||(C.x1==-1))
     {
-//      print_point(l0, l1, l2, A, B, C, false);
-      //cout << "ERROR: " << l0 << " " << l1 << " " << l2 << endl;
-//      out_debug_wtf.close();
       rez_of_check = false;
     }
-
   }
-  //print_fi(save_rez, k);
-//  out_debug_wtf.close();
     return rez_of_check;
-}
-
-vector<int> gen_rand_fi(int k)
-{
-  vector<int> fi(k*k, 0);
-  for (int i = 0; i < fi.size(); ++i)
-  {  
-    fi[i] = rand()%k;
-  }
-  return fi;
 }
 
 
@@ -146,7 +113,6 @@ vector<vector<int>> read_bounds(int n, int number)
 {
   ifstream in;
   in.open("list_of_bounds.txt");
-  //int one;
   vector<int> tmp(n, 0);
   vector<vector<int>> res(number, tmp); 
   for (int i = 0; i < number; ++i)
@@ -162,17 +128,9 @@ int main()
 {
   int k = 4;
   int number_of_tryes;
-  //cin >> k;
   int n = 2;
-  int d = 2;
   vector <int> fi(k*k, 0);//k^n
-  //vector <int> list_of_temp_fi(4096, fi);
-  //vector <int> tmp(4, 0);
-  //vector <int> cl_of_eq(76, tmp);
   auto cl_of_eq = read_bounds(k, 76);
-  //ofstream out;
-//  out.open("out_lin.txt");
-  //bool flag = false;
   int success = -1;
   
   int kolvo_bound = cl_of_eq.size();
@@ -204,9 +162,8 @@ int main()
 	}
   
   if (success >= 0)
-    cout << "success" << endl;
+    cout << "SUCCESS" << endl;
   else
-    cout << "NO success" << endl;
-  //out.close();
+    cout << "NO SUCCESS" << endl;
   return 0;
 }
